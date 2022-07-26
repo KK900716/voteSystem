d451bbafa142508c7b4f190b8b2c3822b7aabe10
Liangjc <443808626@qq.com>
2022-07-26 11:04:32 +0800

    feat:create framework

--- encryptionSystem/src/main/java/com/sics/instance/AbstractEncryption.java
+++ encryptionSystem/src/main/java/com/sics/instance/AbstractEncryption.java
@@ -7,5 +7,6 @@ package com.sics.instance;
  * @version 2022~07~25~15:34
  */
 public abstract class AbstractEncryption {
-
+    public abstract String encryption(String value);
+    public abstract String decryption(String code);
 }
